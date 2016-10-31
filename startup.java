07ec02844668fa149a50320eb61f29618c7c6003
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-31 10:37:22 -0500

    Create startup.java

---
+++ startup.java
@@ -0,0 +1,18 @@
+ /*****************************************************\
+||-------Project 'C' Is Owned by: The 2nd Player-------||
+||---Lead Developer & Project Manager: Jackson Tweet---||
+||-----Beta Testers:(I will put a link here later)-----||
+ \*****************************************************/
+
+let welcome = "Welcome to the Developement version of Project c";
+let m2 = "If you are here arent a teser and you managed to compile and start this app!";
+let m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
+let m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file.";
+
+print("" + welcome)
+print("")
+print("" + m2)
+print("")
+print("" + m3)
+print("")
+print("" + m4)
