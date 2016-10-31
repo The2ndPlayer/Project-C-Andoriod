ded2564d0db242caa31909f86ca84ca490f47546
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-31 10:38:32 -0500

    Update startup.java

--- startup.java
+++ startup.java
@@ -4,15 +4,15 @@
 ||-----Beta Testers:(I will put a link here later)-----||
  \*****************************************************/
 
-let welcome = "Welcome to the Developement version of Project c";
-let m2 = "If you are here arent a teser and you managed to compile and start this app!";
-let m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
-let m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file.";
+string welcome = "Welcome to the Developement version of Project c";
+string m2 = "If you are here arent a teser and you managed to compile and start this app!";
+string m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
+string m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file.";
 
-print("" + welcome)
-print("")
-print("" + m2)
-print("")
-print("" + m3)
-print("")
-print("" + m4)
+system.out.println("" + welcome)
+system.out.printlnprint("")
+system.out.println("" + m2)
+system.out.println("")
+system.out.println("" + m3)
+system.out.println("")
+system.out.println("" + m4)
