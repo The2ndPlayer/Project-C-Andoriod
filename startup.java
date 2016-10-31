8a99475917f958b19e283d48164c6ca5e7beab24
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-31 14:26:57 -0500

    Update startup.java

--- startup.java
+++ startup.java
@@ -9,9 +9,9 @@ public class StartUp
   public static void main(String[] args)
   {
    string welcome = "Welcome to the Developement version of Project C for Java";
-   string m2 = "If you are here arent a tester and you managed to compile and start this app!";
-   string m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
-   string m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file.";
+   string m2 = "If you are here arent a tester and you managed to compile and start this app";
+   string m3 = "good job!";
+   string m4 = "Please remeber that this proududct is under the MIT licence";
 
    system.out.println("" + welcome)
    system.out.printlnprint("")
